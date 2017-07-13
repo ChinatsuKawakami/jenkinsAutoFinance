bbd2f2c994e4f9b2f46a80b8d01d341a37d4c930
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-07-12 21:16:09 -0400

    1.5 fixed the code to access AMZN

--- autoFinance.py
+++ autoFinance.py
@@ -2,7 +2,7 @@
 #Author Chinatsu Kawakami
 #Name: autoFinance.py
 #Description This is the automation program to get stock information from Yahoo.ca with Python and jenkins
-#version :1.4 Add code to check CAD/US chart
+#version :1.5 fixed the code to access AMZN
 
 
 #!/bin/bash -ex
@@ -13,6 +13,7 @@ import time
 import lxml.html
 
 driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+
 # Set the size of window
 driver.maximize_window()
 # Access Google Chrome
@@ -21,18 +22,19 @@ driver.get('https://ca.yahoo.com/')
 # Show the title
 print('title of the page : '+driver.title)
 
-# push Finance link
-element=driver.find_element_by_css_selector("a[href='https://ca.finance.yahoo.com/']")
-element.click()
 
 #search specific stock info 'AMZN'
-
+driver.implicitly_wait(20)
 search_info = driver.find_element_by_name('p')
 search_info.send_keys('AMZN')
 search_info.send_keys(Keys.ENTER)
 
+driver.implicitly_wait(5)
 # push the link to get more info
 
-get_moreInfo = driver.find_element_by_css_selector("a[href='/quote/CADUSD=X?p=CADUSD=X']")
-get_moreInfo.send_keys(Keys.ENTER)
+#get_moreInfo = driver.find_element_by_css_selector("a[href='/quote/CADUSD=X?p=CADUSD=X']")
+#get_moreInfo.click()
 
+# get href from a tag
+
+search_info.find_element_by_css_selector("a[href='http://www.nasdaq.com/symbol/amzn']").click()
